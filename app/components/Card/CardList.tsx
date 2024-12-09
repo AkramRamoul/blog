f2a49cd6ef456434d57f9bcb3a731d9eb52feaf1
AkramRamoul <ahmedakramramoul@gmail.com>
2024-12-09 16:37:38 +0100

    using swr

--- app/components/Card/CardList.tsx
+++ app/components/Card/CardList.tsx
@@ -1,27 +1,43 @@
+"use client";
+
 import React from "react";
+import useSwr from "swr";
 import Pagination from "../Pagination";
 import Card from "./Card";
 import { Post } from "@prisma/client";
+import axios from "axios";
+import { ImSpinner2 } from "react-icons/im";
 
-const getData = async (page: number, cat: string) => {
-  const res = await fetch(
-    `http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`,
-    {
-      cache: "no-store",
-    }
-  );
-  const data = await res.json();
-  return data;
-};
 interface CardListProps {
   page: number;
   cat: string;
 }
 
-async function CardList({ page, cat }: CardListProps) {
-  const { posts, count } = await getData(page, cat);
+const fetcher = async (url: string) => {
+  const res = await axios.get(url);
+  return res.data;
+};
 
+function CardList({ page, cat }: CardListProps) {
   const POST_PER_PAGE = 2;
+
+  // Use SWR to fetch posts and count
+  const { data, error, isLoading } = useSwr(
+    `/api/posts?page=${page}&cat=${cat || ""}`,
+    fetcher,
+    { revalidateOnFocus: false }
+  );
+
+  // Handle loading and error states
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center w-full h-full">
+        <ImSpinner2 className="animate-spin h-12 w-12" />
+      </div>
+    );
+  if (error) return <div className="flex-[5]">Failed to load data</div>;
+
+  const { posts, count } = data;
 
   const hasPreviousPage = POST_PER_PAGE * (page - 1) > 0;
   const hasNextPage = POST_PER_PAGE * page < count;
