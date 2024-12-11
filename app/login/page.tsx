c98cd8c7edca452b92ea7f2b7c6a74b927acdb85
AkramRamoul <ahmedakramramoul@gmail.com>
2024-12-11 14:44:35 +0100

    fix page not loading

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,11 @@ import Login from "@/components/Login";
 import { Suspense } from "react";
 
 function LoginPage() {
-  <Suspense fallback={<div>Loading...</div>}>
-    <Login />
-  </Suspense>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Login />
+    </Suspense>
+  );
 }
 export default LoginPage;
 export const dynamic = "force-dynamic";
